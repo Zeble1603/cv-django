0b030f7dbe15aea5bfda7344c69fa5bb47c54092
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-09-20 18:09:23 +0200

    modified customloginform

--- cv/recommendation/forms.py
+++ cv/recommendation/forms.py
@@ -17,5 +17,9 @@ class CustomLoginForm(AuthenticationForm):
 
     def __init__(self, *args, **kwargs):
         super(CustomLoginForm, self).__init__(*args, **kwargs)
+        username = forms.CharField(widget=forms.TextInput(
+            attrs={'placeholder':'Username', 'id':'username'}))
+        password = forms.CharField(widget=forms.TextInput(
+            attrs={'placeholder':'Password', 'id':'password'}))
         self.fields['username'].label = False
         self.fields['password'].label = False    
