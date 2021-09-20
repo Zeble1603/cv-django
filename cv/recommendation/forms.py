76fb329fb39504ab0753d585bef454235bdfda28
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-09-20 18:03:57 +0200

    create recommandation and login forms

--- cv/recommendation/forms.py
+++ cv/recommendation/forms.py
@@ -1,1 +1,21 @@
 from django import forms
+from .models import Recommendation
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+
+class RecommendationForm(forms.ModelForm):
+    
+    class Meta:
+        model = Recommendation
+        fields = ("first_name","last_name","job","relationship",
+        "comment")
+
+class CustomLoginForm(AuthenticationForm):
+    class Meta: 
+        model = User
+        fiels = ['username','password']
+
+    def __init__(self, *args, **kwargs):
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = False
+        self.fields['password'].label = False    
